9c2897f61065ca620b5b0968edd2e7188fa81bdc
MargoGV <MV.Gubina@yandex.ru>
2015-09-29 11:10:17 +0300

    first commit

--- A.py
+++ A.py
@@ -1,2 +1,4 @@
 A = list(map(int, input().split()))
 print(A[::2])
+print(max(A), A.index(max(A)))
+print(A[::-1])
--- B.py
+++ B.py
@@ -1,2 +1,7 @@
-A = list(map(int, input().split()))
-print(max(A), A.index(max(A)))
+A = [1, 2, 3, 4, 5]
+A.insert(0, A.pop())
+print(A)
+
+for i in range(0, len(A)-1, 2):
+    A[i], A[i+1] = A[i+1], A[i]
+print(A)
