b0ff986aa846f178cf345528a24639fbb86b24a5
MargoGV <MV.Gubina@yandex.ru>
2015-09-29 12:06:05 +0300

    first commit

---
+++ ex5-1.py
@@ -0,0 +1,4 @@
+from random import *
+output = open('output.txt', 'w')
+for i in range(1000000):
+    print(randint(0, 100), file = output)
---
+++ ex5-2.py
@@ -0,0 +1,5 @@
+from random import *
+output1 = open('output1.txt', 'w')
+for i in range(1000000):
+    print(randint(0, 10000)/100, file = output1)
+
