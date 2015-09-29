e1c39b17df4a429e0e0d9560b8daa81f010a86a0
MargoGV <MV.Gubina@yandex.ru>
2015-09-29 11:41:18 +0300

    first commit

--- B.py
+++ B.py
@@ -5,3 +5,11 @@ print(A)
 for i in range(0, len(A)-1, 2):
     A[i], A[i+1] = A[i+1], A[i]
 print(A)
+
+
+A = [1, 2, 2, 2, 3, 3]
+for i in range (len(A)):
+    if A.count(A[i]) == 1:
+        print(A[i])
+
+
